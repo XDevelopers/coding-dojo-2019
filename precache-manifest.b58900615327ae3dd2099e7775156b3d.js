ff913f21921b431ca57d49afc769847381206f5a
Marcio Lourenco <xquema@gmail.com>
2019-03-22 10:47:17 -0300

    deploy

---
+++ precache-manifest.b58900615327ae3dd2099e7775156b3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/coding-dojo-2019/robots.txt"
+  },
+  {
+    "revision": "09940f65368b1d585208",
+    "url": "/coding-dojo-2019/js/chunk-vendors.f7979d81.js"
+  },
+  {
+    "revision": "b91b80ac514f2268f7f1",
+    "url": "/coding-dojo-2019/js/app.f1f19543.js"
+  },
+  {
+    "revision": "26fee0059ec098d5f609",
+    "url": "/coding-dojo-2019/js/about.d288b4f1.js"
+  },
+  {
+    "revision": "13b4ece50333bc1ec7773f737258f996",
+    "url": "/coding-dojo-2019/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/coding-dojo-2019/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b91b80ac514f2268f7f1",
+    "url": "/coding-dojo-2019/css/app.47e54b2a.css"
+  }
+];
